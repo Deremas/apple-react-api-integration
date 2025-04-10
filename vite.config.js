dab518f3d579ab8f3c63b5b3cfe7ef5cc8240f2f
Dereje Masresha <deremas27@gmail.com>
2025-04-10 03:01:53 +0300

    apple with react

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/apple-react-api-integration/',
   plugins: [react()],
   server: {
     port: 2244,
